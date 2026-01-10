9fc0f1c93e788ca600f4c1d86fb46b18f2eb2c4b
geoorgeous <gh@georgemcdonagh.dev>
2026-01-10 01:10:34 +0000

    logging category fixes

--- src/logging.c
+++ src/logging.c
@@ -5,7 +5,6 @@
 
 #include "logging.h"
 
-#define CX_LOG_CAT_MIN_LEVEL_DEFAULT CX_LOG_ALL
 #define CX_LOG_CAT_DELIM ':'
 #define CX_LOG_CAT_STR_BUF_LEN 2048
 #define CX_MAX_LOG_CATS 128
@@ -20,7 +19,6 @@ static const char* k_log_level_strings[] = {
     CX_LOG_LABEL_DEBUG
 };
 
-static int cmp_log_levels(int min_level, int level);
 static struct cx_log_cat* get_log_cat(const char* s_cat, size_t cat_len);
 static int is_log_visible(const char* s_cat, int level);
 static void print_prefix(FILE* p_file, int log_level, const char* s_category);
@@ -34,7 +32,7 @@ struct cx_log_cat {
 static char              log_cat_str_buf[CX_LOG_CAT_STR_BUF_LEN];
 static char*             p_log_cat_str_buf_next = log_cat_str_buf;
 static struct cx_log_cat log_cats[CX_MAX_LOG_CATS];
-static int               log_cat_global_min_level = CX_LOG_CAT_MIN_LEVEL_DEFAULT;
+static int               log_cat_global_min_level = CX_LOG_INFO;
 
 void cx_log(int level, const char* s_cat, const char* s_msg) {
 	if (!is_log_visible(s_cat, level)) {
@@ -80,28 +78,42 @@ void cx_log_fmt(int level, const char* s_cat, const char* s_fmt, ...) {
 void cx_log_cat_set(const char *s_cat, int min_level) {
 	if (!s_cat) {
 		log_cat_global_min_level = min_level;
+		CX_DBG_LOG_FMT(CX_LOG_CAT_LOGGING, "Log category minimum level set: (Global) -> %d\n", min_level);
 		return;
 	}
 
 	struct cx_log_cat* p_cat = get_log_cat(s_cat, 0);
-	p_cat->min_level = min_level;
-}
 
-int cmp_log_levels(int min_level, int level) {
-	return min_level >= 0 && min_level <= level && log_cat_global_min_level <= level;
+	if (p_cat->s_display_str == 0) {
+		*p_cat = (struct cx_log_cat) {
+			.s_display_str = p_log_cat_str_buf_next,
+			.display_str_len = strlen(s_cat),
+			.min_level = min_level
+		};
+
+		memcpy(p_log_cat_str_buf_next, s_cat, p_cat->display_str_len);
+		p_log_cat_str_buf_next += p_cat->display_str_len;
+		*p_log_cat_str_buf_next++ = '\0';
+
+		CX_DBG_LOG_FMT(CX_LOG_CAT_LOGGING, "New log category: '%s' min_level=%d\n", s_cat, min_level);
+	} else {
+		p_cat->min_level = min_level;
+		CX_DBG_LOG_FMT(CX_LOG_CAT_LOGGING, "Log category minimum level set: '%s' -> %d\n", s_cat, min_level);
+	}
 }
 
 struct cx_log_cat* get_log_cat(const char* s_cat, size_t cat_len) {
 	struct cx_log_cat* p_cat = log_cats;
 
 	while (p_cat->s_display_str) {
-		const int b_match = 
-			cat_len == 0 ?
+		const int b_match =
+			(cat_len == 0 || cat_len == p_cat->display_str_len) &&
+			p_cat->display_str_len != cat_len ?
 			strcmp(s_cat, p_cat->s_display_str) == 0 :
 			strncmp(
 				s_cat,
 				p_cat->s_display_str,
-				cat_len < p_cat->display_str_len ? cat_len : p_cat->display_str_len) == 0;
+				cat_len > p_cat->display_str_len ? cat_len : p_cat->display_str_len) == 0;
 
 		if (b_match) {
 			return p_cat;
@@ -109,41 +121,34 @@ struct cx_log_cat* get_log_cat(const char* s_cat, size_t cat_len) {
 
 		p_cat++;
 	}
-
-	*p_cat = (struct cx_log_cat) {
-		.s_display_str = p_log_cat_str_buf_next,
-		.display_str_len = cat_len ? cat_len : strlen(s_cat),
-		.min_level = CX_LOG_CAT_MIN_LEVEL_DEFAULT
-	};
-
-	memcpy(p_log_cat_str_buf_next, s_cat, p_cat->display_str_len);
-	p_log_cat_str_buf_next += p_cat->display_str_len;
-	*p_log_cat_str_buf_next++ = '\0';
-
+	
 	return p_cat;
 }
 
 int is_log_visible(const char* s_cat, int level) {
-	if (log_cat_global_min_level < 0) {
+	if (log_cat_global_min_level > level || log_cat_global_min_level < 0) {
 		return 0;
 	}
 
 	if (s_cat == 0) {
-		return log_cat_global_min_level <= level;
+		return 1;
 	}
 
 	const char* p = s_cat;
 	size_t len = 0;
 
-	while (*p) {
-		if (*p == CX_LOG_CAT_DELIM && !cmp_log_levels(get_log_cat(s_cat, len)->min_level, level)) {
-			return 0;
+	while (1) {
+		if (*p == CX_LOG_CAT_DELIM || *p == '\0') {
+			const struct cx_log_cat* p_cat = get_log_cat(s_cat, len);
+			const int cat_min_level = p_cat->s_display_str ? p_cat->min_level : CX_LOG_ALL;
+			const int b_visible = cat_min_level <= level && cat_min_level >= 0;  
+			if (!b_visible || *p == '\0') {
+				return b_visible;
+			}
 		}
 		++len;
 		p++;
 	}
-
-	return cmp_log_levels(get_log_cat(s_cat, len)->min_level, level);
 }
 
 void print_prefix(FILE* p_file, int log_level, const char* s_category) {
@@ -151,7 +156,7 @@ void print_prefix(FILE* p_file, int log_level, const char* s_category) {
 
     time_t timestamp = time(0);
     struct tm* tm = localtime(&timestamp);
-    (void)strftime(timestamp_str_buffer, 20, "%Y-%m-%d %H:%M:%S", tm);
+    (void)strftime(timestamp_str_buffer, sizeof(timestamp_str_buffer), "%Y-%m-%d %H:%M:%S", tm);
 
     if (s_category) {
         (void)fprintf(p_file, "[%s] %s: (%s) ", timestamp_str_buffer, k_log_level_strings[log_level], s_category);
