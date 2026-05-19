450a0e04f9df6868941b345638e8679018d5984c
George McDonagh <mail@georgemcdonagh.dev>
2026-05-19 21:05:58 +0100

    cx_color macro tidying

--- src/cx_color.h
+++ src/cx_color.h
@@ -3,13 +3,13 @@
 
 #include <stdint.h>
 
-#define CX_U32_R8G8B8A8_R(U32) ((U32 >> 24) & 0xff)
-#define CX_U32_R8G8B8A8_G(U32) ((U32 >> 16) & 0xff)
-#define CX_U32_R8G8B8A8_B(U32) ((U32 >>  8) & 0xff)
-#define CX_U32_R8G8B8A8_A(U32) ( U32        & 0xff)
+#define CX_U32_R8G8B8A8_R(U32) (((U32) >> 24) & 0xff)
+#define CX_U32_R8G8B8A8_G(U32) (((U32) >> 16) & 0xff)
+#define CX_U32_R8G8B8A8_B(U32) (((U32) >>  8) & 0xff)
+#define CX_U32_R8G8B8A8_A(U32) ( (U32)        & 0xff)
 #define CX_U32_R8G8B8A8(R8, G8, B8, A8) ((u32_r8g8b8a8)(((R8) << 24) | ((G8) << 16) | ((B8) << 8) | (A8)))
 
-#define CX_U8_FROM_F32_PERCENT(F32) ((uint8_t)(F32 * 0xff))
+#define CX_U8_FROM_F32_PERCENT(F32) ((uint8_t)((F32) * 0xff))
 #define CX_F32_PERCENT_FROM_U8(U8) (((float)(U8)) / 0xff)
 
 #define CX_COLOR_NONE                 ((u32_r8g8b8a8)0x00000000)
