108ec398da658a61714cc573416ed55a8f92cb69
George McDonagh <mail@georgemcdonagh.dev>
2026-05-21 00:03:51 +0100

    cx_macro naming fix

--- src/cx_macro.h
+++ src/cx_macro.h
@@ -4,8 +4,8 @@
 #define CX_STRINGIFY_INTERNAL(X) #X
 #define CX_STRINGIFY(X) CX_STRINGIFY_INTERNAL(X)
 
-#define CX_BSEARCH(ARRAY, NUM, KEY, F_CMP_KEY, P_OUT_INDEX, P_B_OUT_FOUND) do {\
-	*(P_B_OUT_FOUND) = 0;\
+#define CX_BSEARCH(ARRAY, NUM, KEY, F_CMP_KEY, P_OUT_INDEX, P_OUT_B_FOUND) do {\
+	*(P_OUT_B_FOUND) = 0;\
 	*(P_OUT_INDEX) = 0;\
 	size_t hi = (NUM);\
 	while (*(P_OUT_INDEX) < hi) {\
@@ -17,7 +17,7 @@
 			hi = mid;\
 		} else {\
 			*(P_OUT_INDEX) = mid;\
-			*(P_B_OUT_FOUND) = 1;\
+			*(P_OUT_B_FOUND) = 1;\
 			break;\
 		}\
 	} } while(0)
