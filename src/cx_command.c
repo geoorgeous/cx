de5b4bd0cf7e176a359551472fc68ed2d6a7ef73
George McDonagh <mail@georgemcdonagh.dev>
2026-05-21 01:04:56 +0100

    fixed argument parsing bug

--- src/cx_command.c
+++ src/cx_command.c
@@ -18,37 +18,40 @@ int cx_command_resolve_args(
 	size_t argc = 0;
 
 	for(const char* p = s_args; p && *p; p++) {
-		if (!isspace(*p)) {
-			s_arg = p;
-
-			if (argc > p_command->num_params) {
-				CX_DBG(CX_LOG(INFO, COMMAND, "usage error: too many arguments\n"));
-				return 0;
-			}
-
-			if (argc == args_max_count) {
-				CX_DBG(CX_LOG(INFO, COMMAND, "internal error: argument buffer too small\n"));
-				return 0;
-			}
-
-			while (*p && !isspace(*p)) {
-				// todo: check for quotes here to preserve spces
-				p++;
-			}
-
-			if (cx_var_parse(
-					&p_command->p_params[argc].desc,
-					s_arg, p - s_arg,
-					&p_out_args->list[argc]) != CX_VAR_PARSE_RESULT_success) {
-				CX_DBG(CX_LOG_FMT(INFO, COMMAND, "parse error: invalid argument %d \"%.*s\" (%s)\n",
-					argc + 1,
-					p - s_arg, s_arg,
-					cx_var_type_str(p_command->p_params[argc].desc.type)));
-				return 0;
-			}
-
-			++argc;
+		if (isspace(*p)) {
+			continue;
 		}
+
+		s_arg = p;
+
+		if (argc == p_command->num_params) {
+			CX_DBG(CX_LOG_FMT(INFO, COMMAND, "usage error: too many arguments, expected %d\n", p_command->num_params));
+			return 0;
+		}
+
+		if (argc == args_max_count) {
+			CX_DBG(CX_LOG(INFO, COMMAND, "internal error: argument buffer too small\n"));
+			return 0;
+		}
+
+		while (p[1] && !isspace(p[1])) {
+			// todo: check for quotes here to preserve spces
+			p++;
+		}
+
+		const size_t len = p - s_arg + 1;
+		if (cx_var_parse(
+				&p_command->p_params[argc].desc,
+				s_arg, len,
+				&p_out_args->list[argc]) != CX_VAR_PARSE_RESULT_success) {
+			CX_DBG(CX_LOG_FMT(INFO, COMMAND, "parse error: invalid argument %d \"%.*s\" (%s)\n",
+				argc + 1,
+				len, s_arg,
+				cx_var_type_str(p_command->p_params[argc].desc.type)));
+			return 0;
+		}
+
+		++argc;
 	}
 
 	if (argc < p_command->num_params && p_command->p_params[argc].b_required) {
