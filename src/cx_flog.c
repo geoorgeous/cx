9d3e50314eb4853371f5a78669e8f443dde0e732
George McDonagh <mail@georgemcdonagh.dev>
2026-05-22 01:37:19 +0100

    cx_flog impl

--- src/cx_flog.c
+++ src/cx_flog.c
@@ -4,110 +4,130 @@
 #include <string.h>
 
 #include "cx_flog.h"
+#include "cx_logging.h"
+#include "cx_str.h"
 
-#define CX_FLOG_TEXT_BUFFER_INITIAL_LEN 1024
-#define CX_FLOG_TEXT_BUFFER_MAX_LEN (1024 * 1024)
-#define CX_FLOG_ENTRY_BUFFER_INITIAL_LEN (CX_FLOG_TEXT_BUFFER_INITIAL_LEN / 100)
-#define CX_FLOG_ENTRY_BUFFER_MAX_LEN (CX_FLOG_TEXT_BUFFER_MAX_LEN / 100)
-#define CX_FLOG_SPAN_BUFFER_INITIAL_LEN CX_FLOG_ENTRY_BUFFER_INITIAL_LEN
-#define CX_FLOG_SPAN_BUFFER_MAX_LEN CX_FLOG_ENTRY_BUFFER_MAX_LEN
+static inline int cx_flog_style_cmp(const struct cx_flog_style* p_a, const struct cx_flog_style* p_b) {
+	return cx_color_f32_cmp(&p_a->color, &p_b->color);
+}
 
-void cx_flog_internal(
-	struct cx_flogger* p_flogger,
-	const struct cx_flog_style* p_style,
-	const char* s_text,
-	size_t text_len);
+static inline void cx_flog_emit_span(struct cx_flog_builder* p_builder) {
+	p_builder->p_spans[p_builder->num_spans] = (struct cx_flog_span) {
+		.start = p_builder->active_style_begin,
+		.len = p_builder->buf_len - p_builder->active_style_begin,
+		.style_id = p_builder->style_stack[p_builder->stack_count - 1]
+	};
+	++p_builder->num_spans;
+}
 
-void cx_flog_init(struct cx_flogger* p_flogger) {
-	*p_flogger = (struct cx_flogger){
-		.entry_buffer_cap = CX_FLOG_ENTRY_BUFFER_INITIAL_LEN,
-		.text_buffer_cap = CX_FLOG_TEXT_BUFFER_INITIAL_LEN,
-		.span_buffer_cap = CX_FLOG_SPAN_BUFFER_INITIAL_LEN
+void cx_flog_end(struct cx_flogger* p_flogger, struct cx_flog_builder* p_builder) {
+	if (p_builder->stack_count && p_builder->active_style_begin < p_builder->buf_len) {
+		cx_flog_emit_span(p_builder);
+	}
+
+	const size_t string_size = p_builder->buf_len + 1;
+	const size_t styles_size = p_builder->num_styles * sizeof(*p_builder->p_styles);
+	const size_t spans_size = p_builder->num_spans * sizeof(*p_builder->p_spans);
+	const size_t entry_size = sizeof(struct cx_flog_entry);
+
+	while(!cx_alloc_ring_can_fit(&p_flogger->ring_strings_, string_size) ||
+		!cx_alloc_ring_can_fit(&p_flogger->ring_styles_, styles_size) ||
+		!cx_alloc_ring_can_fit(&p_flogger->ring_spans_, spans_size) ||
+		!cx_alloc_ring_can_fit(&p_flogger->ring_entries_, entry_size)) {
+		
+		cx_alloc_ring_pop(&p_flogger->ring_strings_);
+		cx_alloc_ring_pop(&p_flogger->ring_styles_);
+		cx_alloc_ring_pop(&p_flogger->ring_spans_);
+		cx_alloc_ring_pop(&p_flogger->ring_entries_);
+	}
+
+	const struct cx_flog_entry new_entry = {
+		.s = cx_alloc_ring_push(
+			&p_flogger->ring_strings_,
+			p_builder->p_buf,
+			string_size,
+			CX_ALLOC_RING_PUSH_POLICY_no),
+		.p_styles = cx_alloc_ring_push(
+			&p_flogger->ring_styles_, 
+			p_builder->p_styles, 
+			styles_size, 
+			CX_ALLOC_RING_PUSH_POLICY_no),
+		.num_styles = p_builder->num_styles,
+		.p_spans = cx_alloc_ring_push(
+			&p_flogger->ring_spans_,
+			p_builder->p_spans,
+			spans_size,
+			CX_ALLOC_RING_PUSH_POLICY_no),
+		.num_spans = p_builder->num_spans
 	};
 
-	const size_t entry_buffer_size = p_flogger->entry_buffer_cap * sizeof(*p_flogger->p_entry_buffer);
-	const size_t text_buffer_size = p_flogger->text_buffer_cap * sizeof(*p_flogger->p_text_buffer);
-	const size_t span_buffer_size = p_flogger->span_buffer_cap * sizeof(*p_flogger->p_span_buffer);
+	CX_LOG(INFO, FLOG, "New entry:\n");
+	CX_LOG_FMT(INFO, DONTCARE, "  content: \"%s\"\n", new_entry.s);
+	CX_LOG_FMT(INFO, DONTCARE, "  styles: %d\n", new_entry.num_styles);
 
-	p_flogger->p_entry_buffer = malloc(entry_buffer_size + text_buffer_size + span_buffer_size);
-	p_flogger->p_text_buffer = (void*)((char*)p_flogger->p_entry_buffer + entry_buffer_size);
-	p_flogger->p_span_buffer = (void*)((char*)p_flogger->p_text_buffer + text_buffer_size);
+	for (size_t i = 0; i < new_entry.num_styles; ++i) {
+		const struct cx_flog_style* p_style = new_entry.p_styles + i;
+		CX_LOG_FMT(INFO, DONTCARE, "    %d: color=[%g, %g, %g, %g]\n",
+			p_style->color.r, p_style->color.g, p_style->color.b, p_style->color.a);
+	}
+
+	CX_LOG_FMT(INFO, DONTCARE, "  spans: %d\n", new_entry.num_styles);
+
+	for (size_t i = 0; i < new_entry.num_spans; ++i) {
+		const struct cx_flog_span* p_span = new_entry.p_spans + i;
+		CX_LOG_FMT(INFO, DONTCARE, "    %d: [%d...%d], style_id=%d\n", p_span->start, p_span->len, p_span->style_id);
+	}
+	
+	cx_alloc_ring_push(&p_flogger->ring_entries_, &new_entry, entry_size, CX_ALLOC_RING_PUSH_POLICY_no);
 }
 
-void cx_flog_free(struct cx_flogger* p_flogger) {
-	free(p_flogger->p_text_buffer);
-	free(p_flogger->p_entry_buffer);
-	free(p_flogger->p_span_buffer);
-	*p_flogger = (struct cx_flogger){0};
+void cx_flog_append(struct cx_flog_builder* p_builder, const char* s) {
+	char* p_start = p_builder->p_buf + p_builder->buf_len;
+	char* p_end = cx_stpcpy(p_start, s);
+	p_builder->buf_len += p_end - p_start;
 }
 
-void cx_flog(struct cx_flogger* p_flogger, const struct cx_flog_style* p_style, const char* s_text) {
-	const size_t text_len = strlen(s_text);
-	cx_flog_internal(p_flogger, p_style, s_text, text_len);
+void cx_flog_append_fmt(struct cx_flog_builder* p_builder, const char* s, ...) {
+	char temp[1024];
+
+	va_list vargs;
+	va_start(vargs, s);
+	const size_t len = vsprintf(temp, s, vargs);
+	va_end(vargs);
+
+	strcpy(p_builder->p_buf + p_builder->buf_len, temp);
+	p_builder->buf_len += len;
 }
 
-void cx_flogf(struct cx_flogger* p_flogger, const struct cx_flog_style* p_style, const char* s_fmt, ...) {
-	char buf[1024];
+void cx_flog_push_style(struct cx_flog_builder* p_builder, struct cx_flog_style style) {
+	if (p_builder->stack_count && p_builder->active_style_begin < p_builder->buf_len) {
+		cx_flog_emit_span(p_builder);
+	}
+	
+	uint8_t style_id = p_builder->num_styles;
+	for (size_t i = 0; i < p_builder->num_styles; ++i) {
+		if (cx_flog_style_cmp(p_builder->p_styles + i, &style)) {
+			style_id = i;
+			break;
+		}
+	}
 
-    va_list args;
-    va_start(args, s_fmt);
+	if (style_id == p_builder->num_styles) {
+		p_builder->p_styles[style_id] = style;
+		++p_builder->num_styles;
+	}
 
-#if defined(__clang__)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wformat-nonliteral"
-#elif defined(__GNUC__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wformat-nonliteral"
-#endif
-	const size_t text_len = vsprintf(buf, s_fmt, args);
-#if defined(__clang__)
-#pragma clang diagnostic pop
-#elif defined(__GNU__)
-#pragma GCC diagnostic pop
-#endif
-
-    va_end(args);
-
-	cx_flog_internal(p_flogger, p_style, buf, text_len);
+	p_builder->style_stack[p_builder->stack_count++] = style_id;
+	p_builder->active_style_begin = p_builder->buf_len;
 }
 
-void cx_flog_end(struct cx_flogger* p_flogger) {
-	if (p_flogger->p_entry_buffer[p_flogger->entry_buffer_head].len == 0) {
-		// Silently ignore empty entries
+void cx_flog_pop_style(struct cx_flog_builder* p_builder) {
+	if (p_builder->stack_count == 0 || p_builder->active_style_begin == p_builder->buf_len) {
 		return;
 	}
 
-	++p_flogger->entry_buffer_len;
+	cx_flog_emit_span(p_builder);
 
-	struct cx_flog_entry* p_entry; // todo: allocate
-
-	*p_entry = (struct cx_flog_entry) {
-		.offset = p_flogger->text_buffer_head,
-		.spans_first = p_flogger->span_buffer_head
-	};
-}
-
-void cx_flog_internal(
-	struct cx_flogger* p_flogger,
-	const struct cx_flog_style* p_style,
-	const char* s_text,
-	size_t text_len) {
-
-	struct cx_flog_entry* p_entry = &p_flogger->p_entry_buffer[p_flogger->entry_buffer_head];
-
-	if (p_style != CX_FLOG_NOSTYLE) {
-		struct cx_flog_span* p_span; // todo: allocate span
-		*p_span = (struct cx_flog_span) {
-			.start = p_entry->len,
-			.len = text_len,
-			.style = *p_style
-		};
-		++p_entry->num_spans;
-	}
-
-	// allocate string
-	// copy
-	
-	p_entry->len += text_len;
-
+	--p_builder->stack_count;
+	p_builder->active_style_begin = p_builder->buf_len;
 }
--- src/cx_flog.h
+++ src/cx_flog.h
@@ -4,7 +4,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "cx_alloc.h"
 #include "cx_color.h"
+
+#define CX_LOG_CAT_FLOG "flog"
 
 #define CX_FLOG_NOSTYLE 0
 
@@ -13,40 +16,44 @@ struct cx_flog_style {
 };
 
 struct cx_flog_span {
-	size_t start;
-	size_t len;
-	struct cx_flog_style style;
+	uint8_t style_id;
+	uint32_t start;
+	uint32_t len;
 };
 
 struct cx_flog_entry {
-	size_t offset;
-	size_t len;
-	size_t spans_first;
-	size_t num_spans;
+	const char* s;
+	const struct cx_flog_style* p_styles;
+	uint8_t num_styles;
+	const struct cx_flog_span* p_spans;
+	uint8_t num_spans;
+};
+
+struct cx_flog_builder {
+	char* p_buf;
+	uint32_t buf_len;
+	struct cx_flog_style* p_styles; // list of style
+	uint8_t num_styles;
+	struct cx_flog_span* p_spans; // list of spans
+	uint8_t num_spans;
+	uint8_t style_stack[32]; // stack of style ids
+	uint8_t stack_count;
+	uint32_t active_style_begin;
 };
 
 struct cx_flogger {
-	char* p_text_buffer;
-	size_t text_buffer_cap;
-	size_t text_buffer_len;
-	size_t text_buffer_head;
-	size_t text_buffer_tail;
-	struct cx_flog_entry* p_entry_buffer;
-	size_t entry_buffer_cap;
-	size_t entry_buffer_len;
-	size_t entry_buffer_head;
-	size_t entry_buffer_tail;
-	struct cx_flog_span* p_span_buffer;
-	size_t span_buffer_cap;
-	size_t span_buffer_len;
-	size_t span_buffer_head;
-	size_t span_buffer_tail;
+	struct cx_alloc_ring ring_strings_;
+	struct cx_alloc_ring ring_styles_;
+	struct cx_alloc_ring ring_spans_;
+	struct cx_alloc_ring ring_entries_;
 };
 
-void cx_flog_init(struct cx_flogger* p_flogger);
-void cx_flog_free(struct cx_flogger* p_flogger);
-void cx_flog(struct cx_flogger* p_flogger, const struct cx_flog_style* p_style, const char* s_text);
-void cx_flogf(struct cx_flogger* p_flogger, const struct cx_flog_style* p_style, const char* s_text, ...);
-void cx_flog_end(struct cx_flogger* p_flogger);
+void cx_flog_end(struct cx_flogger* p_flogger, struct cx_flog_builder* p_builder);
+
+void cx_flog_append(struct cx_flog_builder* p_builder, const char* s);
+void cx_flog_append_fmt(struct cx_flog_builder* p_builder, const char* s, ...);
+void cx_flog_push_style(struct cx_flog_builder* p_builder, struct cx_flog_style style);
+void cx_flog_pop_style(struct cx_flog_builder* p_builder);
+
 
 #endif
