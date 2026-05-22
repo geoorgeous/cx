dfc6487b59eb547953cf0483cdf78ba4622f5d5a
George McDonagh <mail@georgemcdonagh.dev>
2026-05-22 01:36:59 +0100

    cx_alloc_ring renames

--- src/cx_alloc.c
+++ src/cx_alloc.c
@@ -2,15 +2,31 @@
 
 #include "cx_alloc.h"
 
-static int cx_alloc_ring_enough_space(const struct cx_alloc_ring* p_ring, size_t size);
+int cx_alloc_ring_can_fit(const struct cx_alloc_ring* p_ring, size_t size) {
+	if (p_ring->entries_count_ == 0) {
+		return 1;
+	}
 
-int cx_alloc_ring_push(struct cx_alloc_ring* p_ring, const void* p_src, size_t size) {
-	if (size > p_ring->buf_cap) {
+	if (p_ring->buf_head_ >= p_ring->buf_tail_) {
+		return p_ring->buf_head_ + size <= p_ring->buf_cap || size <= p_ring->buf_tail_;
+	}
+
+	return p_ring->buf_head_ + size < p_ring->buf_tail_;
+}
+
+const void* cx_alloc_ring_push(
+	struct cx_alloc_ring* p_ring,
+	const void* p_src,
+	size_t size,
+	enum cx_alloc_ring_push_policy policy) {
+	
+	if (size > p_ring->buf_cap ||
+		(!cx_alloc_ring_can_fit(p_ring, size) && policy == CX_ALLOC_RING_PUSH_POLICY_no)) {
 		return 0;
 	}
 
-	while(!cx_alloc_ring_enough_space(p_ring, size)) {
-		cx_alloc_ring_evict_oldest(p_ring);
+	while(!cx_alloc_ring_can_fit(p_ring, size)) {
+		cx_alloc_ring_pop(p_ring);
 	}
 	
 	if (p_ring->buf_head_ + size > p_ring->buf_cap) {
@@ -24,11 +40,20 @@ int cx_alloc_ring_push(struct cx_alloc_ring* p_ring, const void* p_src, size_t s
 		.size = size
 	};
 
+	const void* p = (char*)p_ring->p_buf + p_ring->buf_head_;
+
 	p_ring->buf_head_ += size;
 	p_ring->entries_head_ = (p_ring->entries_head_ + 1) % p_ring->entries_cap_;
 	++p_ring->entries_count_;
 
-	return 1;
+	return p;;
+}
+
+void cx_alloc_ring_pop(struct cx_alloc_ring* p_ring) {
+	const struct cx_alloc_ring_entry* p_entry = p_ring->p_entries_ + p_ring->entries_tail_;
+	p_ring->buf_tail_ = (p_entry->offset + p_entry->size) % p_ring->buf_cap;
+	p_ring->entries_tail_ = (p_ring->entries_tail_ + 1) % p_ring->entries_cap_;
+	--p_ring->entries_count_;
 }
 
 const void* cx_alloc_ring_get(const struct cx_alloc_ring* p_ring, size_t age, size_t* p_out_size) {
@@ -36,23 +61,4 @@ const void* cx_alloc_ring_get(const struct cx_alloc_ring* p_ring, size_t age, si
 	const struct cx_alloc_ring_entry* p_entry = p_ring->p_entries_ + index;
 	*p_out_size = p_entry->size;
 	return (char*)p_ring->p_buf + p_entry->offset;
-}
-
-void cx_alloc_ring_evict_oldest(struct cx_alloc_ring* p_ring) {
-	const struct cx_alloc_ring_entry* p_entry = p_ring->p_entries_ + p_ring->entries_tail_;
-	p_ring->buf_tail_ = (p_entry->offset + p_entry->size) % p_ring->buf_cap;
-	p_ring->entries_tail_ = (p_ring->entries_tail_ + 1) % p_ring->entries_cap_;
-	--p_ring->entries_count_;
-}
-
-int cx_alloc_ring_enough_space(const struct cx_alloc_ring* p_ring, size_t size) {
-	if (p_ring->entries_count_ == 0) {
-		return 1;
-	}
-
-	if (p_ring->buf_head_ >= p_ring->buf_tail_) {
-		return p_ring->buf_head_ + size <= p_ring->buf_cap || size <= p_ring->buf_tail_;
-	}
-
-	return p_ring->buf_head_ + size < p_ring->buf_tail_;
 }
--- src/cx_alloc.h
+++ src/cx_alloc.h
@@ -3,6 +3,11 @@
 
 #include <stddef.h>
 
+enum cx_alloc_ring_push_policy {
+	CX_ALLOC_RING_PUSH_POLICY_auto,
+	CX_ALLOC_RING_PUSH_POLICY_no
+};
+
 struct cx_alloc_ring_entry {
 	size_t offset;
 	size_t size;
@@ -20,10 +25,37 @@ struct cx_alloc_ring {
 	size_t entries_count_;
 };
 
-int cx_alloc_ring_push(struct cx_alloc_ring* p_ring, const void* p_src, size_t size);
+#define CX_ALLOC_RING_INIT_ARRAYS(P_RING, BUF, ENTRIES) (\
+	cx_alloc_ring_init(\
+		P_RING,\
+		BUF, sizeof(BUF),\
+		ENTRIES, CX_ARRAY_LEN(ENTRIES))\
+	)
+
+static inline void cx_alloc_ring_init(
+	struct cx_alloc_ring* p_ring,
+	void* p_buf, size_t buf_size,
+	struct cx_alloc_ring_entry* p_entries,
+	size_t entries_len) {
+
+	*p_ring = (struct cx_alloc_ring) {
+		.p_buf = p_buf,
+		.buf_cap = buf_size,
+		.p_entries_ = p_entries,
+		.entries_cap_ = entries_len
+	};
+}
+
+int cx_alloc_ring_can_fit(const struct cx_alloc_ring* p_ring, size_t size);
+
+const void* cx_alloc_ring_push(
+	struct cx_alloc_ring* p_ring,
+	const void* p_src,
+	size_t size,
+	enum cx_alloc_ring_push_policy policy);
+
+void cx_alloc_ring_pop(struct cx_alloc_ring* p_ring);
 
 const void* cx_alloc_ring_get(const struct cx_alloc_ring* p_ring, size_t age, size_t* p_out_size);
-
-void cx_alloc_ring_evict_oldest(struct cx_alloc_ring* p_ring);
 
 #endif
