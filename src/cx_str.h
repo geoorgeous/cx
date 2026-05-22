df6b826fc52f91c3b790a6fb051cbbccfb776ba1
George McDonagh <mail@georgemcdonagh.dev>
2026-05-22 01:31:10 +0100

    added cx_stpcpy

--- src/cx_str.h
+++ src/cx_str.h
@@ -3,6 +3,20 @@
 
 #include <stddef.h>
 
-int cx_strcmp_n(const char* s_a, const char* p_b, size_t n);
+static inline int cx_strcmp_n(const char* s_a, const char* p_b, size_t len) {
+	while(len-- && *s_a && *s_a == *p_b) {
+		s_a++;
+		p_b++;
+	}
+
+	return (unsigned char)(*s_a) - (unsigned char)((len != ((size_t)-1)) ? *p_b : '\0');
+}
+
+static inline char* cx_stpcpy(char* p_dst, const char* s_src) {
+	while((*p_dst = *s_src)) {
+		++p_dst, ++s_src;
+	}
+	return p_dst;
+}
 
 #endif
