205f721a80c5bedaeb3be79c4be4a52c164df29b
George McDonagh <mail@georgemcdonagh.dev>
2026-05-24 16:41:11 +0100

    added CX_MALLOC and friends

--- src/cx_alloc.h
+++ src/cx_alloc.h
@@ -1,7 +1,60 @@
 #ifndef CX_ALLOC_H
 #define CX_ALLOC_H
 
+#include <stdlib.h>
 #include <stddef.h>
+
+#include "cx_dbg.h"
+#include "cx_logging.h"
+#include "cx_macro.h"
+
+#define CX_LOG_CAT_ALLOC "alloc"
+
+#define CX_MALLOC(SIZE) cx_malloc(SIZE, __FILE__, __LINE__, __func__)
+#define CX_CALLOC(SIZE) cx_calloc(SIZE, __FILE__, __LINE__, __func__)
+#define CX_REALLOC(P, SIZE) cx_realloc(P, SIZE, __FILE__, __LINE__, __func__)
+#define CX_FREE(P) free(P)
+
+static inline void* cx_malloc(size_t size, const char* s_file, int line, const char* s_func) {
+	void* p = malloc(size);
+
+CX_DBG(
+	if (!p) {
+		CX_LOG_FMT(ERROR, ALLOC, "Failed to allocate %"CX_PRI_SIZE" bytes! %s:%d (%s)\n",
+			size, s_file, line, s_func);
+		abort();
+	}
+);
+
+	return p;
+}
+
+static inline void* cx_calloc(size_t size, const char* s_file, int line, const char* s_func) {
+	void* p = calloc(1, size);
+
+CX_DBG(
+	if (!p) {
+		CX_LOG_FMT(ERROR, ALLOC, "Failed to allocate %"CX_PRI_SIZE" bytes! %s:%d (%s)\n", size, s_file, line, s_func);
+		abort();
+	}
+);
+
+	return p;
+}
+
+static inline void* cx_realloc(void* p, size_t size, const char* s_file, int line, const char* s_func) {
+	p = realloc(p, size);
+
+CX_DBG(
+	if (!p) {
+		CX_LOG_FMT(ERROR, ALLOC, "Failed to reallocate %"CX_PRI_SIZE" bytes! %s:%d (%s)\n",
+			size, s_file, line, s_func);
+		abort();
+	}
+);
+
+	return p;
+}
 
 enum cx_alloc_ring_push_policy {
 	CX_ALLOC_RING_PUSH_POLICY_auto,
