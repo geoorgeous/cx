0c9e896ec95776156c9fcdfd125b0e255e0f1371
George McDonagh <mail@georgemcdonagh.dev>
2026-05-26 01:05:06 +0100

    cx_world_renderer use cx_render_pass

--- src/cx_world_renderer.c
+++ src/cx_world_renderer.c
@@ -1,9 +1,9 @@
 #include "cx_cmp_static_mesh.h"
 #include "cx_dbg.h"
-#include "cx_gfx_mesh.h"
 #include "cx_gfx_program.h"
 #include "cx_image.h"
 #include "cx_io.h"
+#include "cx_render_pass.h"
 #include "cx_texture.h"
 #include "cx_world.h"
 #include "cx_world_renderer.h"
@@ -22,6 +22,8 @@ static struct cx_gfx_program_param_buffer program_pbuffer_camera;
 static struct cx_gfx_program_param_buffer program_pbuffer_object;
 static struct cx_gfx_program_param_buffer program_pbuffer_material;
 
+static struct cx_render_pass render_pass;
+
 static struct cx_gfx_texture texture_white_1x1;
 
 static void cx_world_renderer_init(void);
@@ -29,17 +31,11 @@ static void cx_world_renderer_init(void);
 void cx_world_renderer_draw(struct cx_world* p_world, const float* p_projection_matrix, const float* p_view_matrix) {
 	cx_world_renderer_init();
 
-	float camera[32];
-	matrix_copy(p_projection_matrix, &camera[0]);
-	matrix_copy(p_view_matrix, &camera[16]);
+	const float material_color[] = { 1, 1, 1 };
 
-	cx_gfx_program_bind(&program);
-	
-	cx_gfx_program_param_block_bind_buffer(&program_pblock_camera, &program_pbuffer_camera, 0, 0);
-	cx_gfx_program_param_block_bind_buffer(&program_pblock_object, &program_pbuffer_object, 0, 0);
-	cx_gfx_program_param_block_bind_buffer(&program_pblock_material, &program_pbuffer_material, 0, 0);
-
-	cx_gfx_program_param_buffer_set(&program_pbuffer_camera, 0, 0, camera);
+	struct cx_gfx_program_opaque_param_binding opaque_bindings[1024];
+	struct cx_render_pass_command commands[1024];
+	size_t num_commands = 0;
 
 	const struct cx_component_pool* p_pool = cx_world_get_component_pool(p_world, &cmp_type_static_mesh);
 	const struct cx_cmp_static_mesh* p_static_meshes = (const void*)p_pool->p_dense_components;
@@ -49,11 +45,6 @@ void cx_world_renderer_draw(struct cx_world* p_world, const float* p_projection_
 		struct transform* p_transform = cx_world_entity_get_transform(p_world, p_pool->p_dense_entities[i]);
 
 		transform_compute_world_trs_matrix(p_transform);
-
-		cx_gfx_program_param_buffer_set(&program_pbuffer_object, 0, 0, p_transform->world_trs_matrix);
-
-		float model_color[] = { 1, 1, 1 };
-		cx_gfx_program_param_buffer_set(&program_pbuffer_material, 0, 0, model_color);
 
 		if (!p_static_mesh->b_loaded_device_meshes) {
 			static_mesh_load_device_meshes(p_static_mesh);
@@ -70,11 +61,33 @@ void cx_world_renderer_draw(struct cx_world* p_world, const float* p_projection_
 					p_gfx_texture = &p_texture->gfx_texture_;
 				}
 			}
+
+			opaque_bindings[num_commands] = (struct cx_gfx_program_opaque_param_binding) {
+				.p_param = &program_opaque_texture_albedo,
+				.p_resource = p_gfx_texture
+			};
 			
-			cx_gfx_program_opaque_param_bind_resource(&program_opaque_texture_albedo, p_gfx_texture);
-			cx_gfx_mesh_draw(&p_static_mesh->p_gfx_meshes[j]);
+			commands[num_commands] = (struct cx_render_pass_command) {
+				.p_mesh = p_static_mesh->p_gfx_meshes + j,
+				.p_object_data = p_transform->world_trs_matrix,
+				.p_material_data = material_color,
+				.p_opaque_params = opaque_bindings + num_commands,
+				.num_opaque_params = 1
+			};
+
+			num_commands++;
 		}
 	}
+
+	float camera[32];
+	matrix_copy(p_projection_matrix, &camera[0]);
+	matrix_copy(p_view_matrix, &camera[16]);
+
+	struct cx_render_pass_data render_pass_data = {
+		.p_data = camera
+	};
+
+	cx_render_pass_execute(&render_pass, &render_pass_data, commands, num_commands);
 }
 
 static void cx_world_renderer_init(void) {
@@ -108,6 +121,16 @@ static void cx_world_renderer_init(void) {
 	cx_gfx_program_param_buffer_create(&program_pbuffer_camera, program_pblock_camera.size_);
 	cx_gfx_program_param_buffer_create(&program_pbuffer_object, program_pblock_object.size_);
 	cx_gfx_program_param_buffer_create(&program_pbuffer_material, program_pblock_material.size_);
+
+	render_pass = (struct cx_render_pass) {
+		.p_program = &program,
+		.p_pass_block = &program_pblock_camera,
+		.p_pass_buffer = &program_pbuffer_camera,
+		.p_object_block = &program_pblock_object,
+		.p_object_buffer = &program_pbuffer_object,
+		.p_material_block = &program_pblock_material,
+		.p_material_buffer = &program_pbuffer_material,
+	};
 
     uint8_t white_pixel[] = { 0xFF, 0xFF, 0xFF };
     struct cx_image white_image = {
