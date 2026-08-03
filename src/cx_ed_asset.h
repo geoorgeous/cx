17d3e94aec1bb6ca31511ec5428d75ae38c8d5e8
George McDonagh <mail@georgemcdonagh.dev>
2026-08-03 20:21:51 +0100

    fixed asset ID generation

--- src/cx_ed_asset.h
+++ src/cx_ed_asset.h
@@ -7,14 +7,14 @@
 
 static inline cx_asset_id cx_ed_asset_generate_id(cx_asset_type type) {
 	const uint32_t id_number =
-		((uint32_t)rand() << 17) +
-		((uint32_t)rand() <<  2) +
-		((uint32_t)rand() >> 13);
-	return
 		(
-			(uint32_t)(((uint8_t)type) << 24) |
-			(id_number & CX_ASSET_IDN_MASK)
+			((uint32_t)rand() << 17) +
+		 	((uint32_t)rand() <<  2) +
+		 	((uint32_t)rand() >> 13)
 		) % CX_ASSET_IDN_MAX;
+	return	
+		(uint32_t)(((uint8_t)type) << 24) |
+		(id_number & CX_ASSET_IDN_MASK);
 }
 
 #endif
