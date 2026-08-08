559f7288a0595e0f41c3d80face8e26ea735cf6e
George McDonagh <mail@georgemcdonagh.dev>
2026-08-08 19:39:01 +0100

    +cx_result UNKNOWN, PERMISSION_DENIED

--- src/cx_result.h
+++ src/cx_result.h
@@ -5,6 +5,7 @@ typedef int cx_result;
 
 #define CX_SUCCESS 0
 
+#define CX_ERROR_UNKNOWN            -1
 #define CX_ERROR_INVALID_ARG         1
 #define CX_ERROR_OUT_OF_MEMORY       2
 #define CX_ERROR_UNSUPPORTED         3
@@ -13,5 +14,6 @@ typedef int cx_result;
 #define CX_ERROR_IO                  6
 #define CX_ERROR_SERIALIZE           7
 #define CX_ERROR_DESERIALIZE         8
+#define CX_ERROR_PERMISSION_DENIED   9
 
 #endif
